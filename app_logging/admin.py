c81d7a296ec9ecfd2289ea2ac36138babf51482e
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-02-02 16:34:54 +0700

    add MailingReport model

--- app_logging/admin.py
+++ app_logging/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_logging.models import MailingReport
+
+
+class ChildReportsInline(admin.TabularInline):
+    model = MailingReport
+    can_delete = False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+@admin.register(MailingReport)
+class MailingReportAdmin(admin.ModelAdmin):
+    list_display = ['report_status', 'report_date_time', 'mailing']
+    list_filter = ['report_status']
+    search_fields = ['report_date_time', 'report_message']
+    inlines = [
+        ChildReportsInline
+    ]
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
--- app_logging/apps.py
+++ app_logging/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AppLoggingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_logging"
+
+    verbose_name = 'Отчеты'
